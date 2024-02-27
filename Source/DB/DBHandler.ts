00f38ec7527d33de08c0b2225df7f085fd93f3e8
G3ntl3ma <luca_eisen@web.de>
2024-02-27 15:09:42 +0100

    Security Leak

--- Source/DB/DBHandler.ts
+++ Source/DB/DBHandler.ts
@@ -6,7 +6,6 @@ import serverConfig from './serverConfig.js';
 
 let mongoose = require('mongoose');
 let localconnection = 'mongodb://127.0.0.1:27017/LoLBotDB'
-let connectionstring = `mongodb+srv://G3ntl3ma:wEA0Yv2PiqnkaD3S@test.p920t4v.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(localconnection).then((result: any) =>{console.log("Connected to Database")})
 
 export async function addGuild(id:string){
