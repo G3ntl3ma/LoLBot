69203ff0f4bddd22a69c9bdc736e2e49f321da83
G3ntl3ma <luca_eisen@web.de>
2024-02-27 14:54:12 +0100

    Fixed a Problem with DB String

--- Source/DB/DBHandler.ts
+++ Source/DB/DBHandler.ts
@@ -5,7 +5,7 @@
 import serverConfig from './serverConfig.js';
 
 let mongoose = require('mongoose');
-let localconnection = 'mongodb://127.0.0.1:27017/testdb'
+let localconnection = 'mongodb://127.0.0.1:27017/LoLBotDB'
 let connectionstring = `mongodb+srv://G3ntl3ma:wEA0Yv2PiqnkaD3S@test.p920t4v.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(localconnection).then((result: any) =>{console.log("Connected to Database")})
 
