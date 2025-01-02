4e03af164839ad3236aa10c03308f677de278b76
G3ntl3ma <luca_eisen@web.de>
2025-01-02 21:33:41 +0100

    added some fixes for bugs

--- Source/events/joinGuild.ts
+++ Source/events/joinGuild.ts
@@ -1,18 +1,23 @@
-import {Events, Guild} from "discord.js";
-import {getGuild} from "../DB/DBHandler";
+import { Events, Guild } from "discord.js";
+import { getGuild } from "../DB/DBHandler";
 
 module.exports = {
-    name: Events.GuildCreate,
-    async execute(guild: Guild ) {
-        //Set a default output channel for the Games
-        // @ts-ignore
-        const guildConfig = await getGuild();
-        const config = new guildConfig({
-            _id: guild.id,
-            out: guild.systemChannelId,
-            timezone: "Etc/GMT+0"
-        })
-        await config.save()
-        console.log("New Server has been successfully added!")
-    },
+  name: Events.GuildCreate,
+  async execute(guild: Guild) {
+    //Set a default output channel for the Games
+
+    try {
+      const guildConfig = await getGuild();
+      const config = new guildConfig({
+        _id: guild.id,
+        out: guild.systemChannelId,
+        timezone: "Etc/GMT+0",
+      });
+      await config.save();
+      console.log("New Server has been successfully added!");
+    } catch {
+      console.log("Guild already added");
+    }
+  },
 };
+
